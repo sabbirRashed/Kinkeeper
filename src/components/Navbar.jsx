846c09e4eed71fb990e1d8a2bff32cfb90efa361
sabbirRashed <srsabbirrahman12@gmail.com>
2026-04-17 00:18:41 +0600

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,57 @@
 import React from 'react';
+import { FiClock } from 'react-icons/fi';
+import { IoHome } from 'react-icons/io5';
+import { LuChartSpline } from 'react-icons/lu';
+import { NavLink } from 'react-router';
 
 const Navbar = () => {
+    const navLinks = <>
+        <li>
+            <NavLink
+                to={'/'}
+                className={({ isActive }) => `mr-5 ${isActive ? "bg-[#244D3F] text-white font-medium" : ""}`}
+            >
+                <IoHome /><span>Home</span>
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to={'/timeLine'}
+                className={({ isActive }) => `mr-5 ${isActive ? "bg-[#244D3F] text-white font-medium" : ""}`}
+            >
+                <FiClock /><span>TimeLine</span>
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+                to={'/stats'}
+                className={({ isActive }) => `mr-5 ${isActive ? "bg-[#244D3F] text-white font-medium" : ""}`}
+            >
+                <LuChartSpline /><span>Stats</span>
+            </NavLink>
+        </li>
+    </>
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        <div className='shadow-sm'>
+            <div className="w-11/12 max-w-325 mx-auto navbar bg-base-100 ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {navLinks}
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex="-1"
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <a className="btn btn-ghost text-2xl">keenkeeper</a>
+                </div>
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navLinks}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2 bg-base-100 w-40 z-1">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
             </div>
         </div>
     );
